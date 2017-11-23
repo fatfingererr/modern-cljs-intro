055a6e5923d50952e70ec45513a66f562422ca10
ffe <fatfingererr@gmail.com>
2017-11-23 19:34:53 +0800

    fix no show problem

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Modern ClojureScript', 
+     subtitle: '導讀 Tutorial 10 ~ 12', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/clojurescript.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'fatfingererr @ ClojureTW 十一月聚會', 
+     company: '', 
+     gplus: 'https://plus.google.com/fatfingererr', 
+     twitter: 'fatfingererr', 
+     www: 'https://fatfingererr.github.io', 
+     github: 'http://github.com/fatfingererr', 
+   }]
+};
